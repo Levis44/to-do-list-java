46d8d2a8923b87e6ca6396e66c8c82080f9080ee
levi <levi.ciarrocchi@gmail.com>
2024-12-25 23:07:46 -0300

    java structure

---
+++ src/main/java/br/com/todo_list/todolist/user/UserController.java
@@ -0,0 +1,31 @@
+package br.com.todo_list.todolist.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+  @Autowired
+  private IUserRepository userRepository;
+
+  @PostMapping("/")
+  public ResponseEntity create(@RequestBody UserModel userModel) {
+    var userAlreadyExists = this.userRepository.findByUsername(userModel.getUsername());
+    
+    if (userAlreadyExists != null) {
+      return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe.");
+    }
+
+    userModel.setPassword(userModel.getPassword());
+
+    var userCreated = this.userRepository.save(userModel);
+
+    return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+  }
+}
